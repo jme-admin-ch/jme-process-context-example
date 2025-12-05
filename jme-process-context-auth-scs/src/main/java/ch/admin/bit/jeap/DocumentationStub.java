883e6d45ce4c7711bd93d8064625a08b5ee3daa3
Laris Jacobs <laris.jacobs@bit.admin.ch>
2025-12-05 14:13:38 +0100

    Add DocumentationStub class for Javadoc generation compliance

---
+++ jme-process-context-auth-scs/src/main/java/ch/admin/bit/jeap/DocumentationStub.java
@@ -0,0 +1,9 @@
+package ch.admin.bit.jeap;
+
+/**
+ * Placeholder class to satisfy Javadoc generation requirements.
+ * This class does not contain any logic.
+ */
+public class DocumentationStub {
+    // Intentionally left blank
+}
